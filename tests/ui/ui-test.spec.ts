71e71ee24fbf52570fa93f23ab2ae25242509780
Manimozhiyan <manimozhiyan1990@gmail.com>
2026-09-21 16:36:00 +0530

    new

--- tests/ui/ui-test.spec.ts
+++ tests/ui/ui-test.spec.ts
@@ -123,6 +123,7 @@ test.describe('Creatio Crm Web Application Automation', () => {
          await loginPage.enterLoginPassword(testData.password);
          await loginPage.clickOnContinueButton();
          await homePage.verifyHomePageIsDisplayed();
-         console.log("Test Ends")
+         console.log("Test Ends");
+         
     })
 })
